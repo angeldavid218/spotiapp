dd88300626098cb4eb126eea33178e6916ce55bb
Angel Serrano <angeldavid218@gmail.com>
2020-02-17 18:27:06 -0600

    artists component

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -11,7 +11,7 @@ export class SpotifyService {
   getQuery(query: string) {
     const url = `https://api.spotify.com/v1/${query}`;
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQAVXNmZYuJQ8RmYZt2Iz7rt1Gw0cNfUBoAHYR1DUHByPs2eIXdyNAMpvnilWw5lYEPxZiFzERBnib5sNl222OS55-v30ybnQsc-vdaEjfUa82Cm4B4bA5qHA5V7L1Wl5fOy3pwj2OIVXiA-'
+      'Authorization': 'Bearer BQCEDzStfLVvSPdKknk5BT6bcse-0MoCM5ZZRyQGGe5dXsL--xH8jqNhYQm8fbXPZFHAcX66_jEfZ6K-JVUXueUwdKAPU514oEY53RJh-bfFsMZvwMxcqIJeiQJAJeXzXwBuh9Y_ouIDLP9e'
     });
     return this.http.get(url, {headers});
   }
@@ -23,11 +23,18 @@ export class SpotifyService {
               }));        
   }
 
-  getArtist(name: string) {
+  getArtists(name: string) {
     return this.getQuery(`search?q=${name}&type=artist&limit=15`)
           .pipe(map((data: any) => {
             return data.artists.items;
           }));
+  }
+
+  getArtist(id: string) {
+    return this.getQuery(`artists/${id}`);
+          /*.pipe(map((data: any) => {
+            return data.artists.items;
+          }));*/
   }
 
 }
